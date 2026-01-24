1df7a02f1249f94826676df706b7183a930dc265
SodaSiz <despairos@proton.me>
2026-01-24 20:08:25 +0100

    Initial commit for compilation via github actions :D

---
+++ build.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+[ ! -e " KernelSU-Next/kernel/setup.sh" ] && git submodule init && git submodule update
+[ ! -d "toolchain" ] && echo  "installing toolchain..." && mkdir -p toolchain && cd toolchain && echo 'Download antman and sync' && bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S=11032023 && echo 'Patch for glibc'
+&& bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") --patch=glibc
+&& echo 'Done'
+
+
+SECONDS=0
+export KBUILD_BUILD_USER=SodaSiz
+
+PATH=$PWD/toolchain/bin:$PATH
+export LLVM_DIR=$PWD/toolchain/bin
+export LLVM=1
+export AnyKernel3=AnyKernel3
+export TIME="$(date "+%Y%m%d")"
+export modpath=${AnyKernel3}/modules/vendor/lib/modules
+
+export ARCH=arm64
+
+if [ -z "$DEVICE" ]; then
+export DEVICE=g84_gdx
+fi
+
+if [[ -z "$KSU" || "$KSU" = "0" ]]; then
+KSU=0
+export KSUSTAT=
+elif [ "$KSU" = "1" ]; then
+CONFIG_KSU=ksu.config
+export KSUSTAT=_KSU
+else
+echo "Error: Set KSU to 0 or 1 to build"
+exit 1
+fi
+export KSU
+
+if [[ -z "$1" || "$1" = "-c" ]]; then
+echo "Clean Build"
+rm -rf out
+elif [ "$1" = "-d" ]; then
+echo "Dirty Build"
+else
+echo "Error: Set $1 to -c or -d"
+exit 1
+fi
+
+ARGS='
+CC=clang
+LD='${LLVM_DIR}/ld.lld'
+ARCH=arm64
+AR='${LLVM_DIR}/llvm-ar'
+NM='${LLVM_DIR}/llvm-nm'
+AS='${LLVM_DIR}/llvm-as'
+CROSS_COMPILE='${LLVM_DIR}/aarch64-linux-gnu'
+CROSS_COMPILE_COMPAT='${LLVM_DIR}/arm-linux-gnueabi'
+OBJCOPY='${LLVM_DIR}/llvm-objcopy'
+OBJDUMP='${LLVM_DIR}/llvm-objdump'
+READELF='${LLVM_DIR}/llvm-readelf'
+OBJSIZE='${LLVM_DIR}/llvm-size'
+STRIP='${LLVM_DIR}/llvm-strip'
+LLVM_AR='${LLVM_DIR}/llvm-ar'
+LLVM_DIS='${LLVM_DIR}/llvm-dis'
+LLVM_NM='${LLVM_DIR}/llvm-nm'
+LLVM=1
+LLVM_IAS=1
+'
+
+make ${ARGS} O=out gki_defconfig vendor/holi_GKI.config vendor/ext_config/lineageos_moto-holi.config vendor/ext_config/moto-holi-bangkk.config
+make ${ARGS} O=out -j$(nproc)
+
+[ ! -e "out/arch/arm64/boot/Image" ] && \
+echo "  ERROR : image binary not found in any of the specified locations , fix compile!" && \
+exit 1
+
+make O=out ${ARGS} -j$(nproc) INSTALL_MOD_PATH=modules INSTALL_MOD_STRIP=1 modules_install
+
+#Clean Up
+rm -rf ${modpath}/*
+rm -rf ${AnyKernel3}/{Image, dtb, dtbo.img}
+rm -rf ${AnyKernel3}/*.zip
+
+#Setup
+mkdir -p ${modpath}
+kver=$(make kernelversion)
+kmod=$(echo ${kver} | awk -F'.' '{print $3}')
+
+#Copy stuff
+cp out/.config ${AnyKernel3}/config
+cp out/arch/arm64/boot/Image ${AnyKernel3}/Image
+cp out/arch/arm64/boot/dtb.img ${AnyKernel3}/dtb
+cp out/arch/arm64/boot/dtbo.img ${AnyKernel3}/dtbo.img
+#cp build.sta/${DEVICE}_modules.blocklist ${modpath}/modules.blocklist
+cp $(find out/modules/lib/modules/5.4* -name '*.ko') ${modpath}/
+cp out/modules/lib/modules/5.4*/modules.{alias,dep,softdep} ${modpath}/
+cp out/modules/lib/modules/5.4*/modules.order ${modpath}/modules.load
+
+#Edit
+sed -i 's/\(kernel\/[^: ]*\/\)\([^: ]*\.ko\)/\/vendor\/lib\/modules\/\2/g' ${modpath}/modules.dep
+#sed -i 's/.*\//.ko/g' ${AnyKernel3}/modules/vendor/lib/modules/modules.load
+#sed -i 's#.*/##; s/\.ko$//' ${AnyKernel3}/modules/vendor/lib/modules/modules.load
+sed -i 's/.*\///; s/\.ko$//' ${modpath}/modules.load
+
+#source build.sta/${DEVICE}_mdconf
+#for useles_modules in "${modules_to_nuke[@]}"; do
+#  grep -vE "$useles_modules" ${modpath}/modules.load > /tmp/templd && mv /tmp/templd ${modpath}/modules.load
+#done
+
+#Zip
+cd ${AnyKernel3}
+zip -r9 O_KERNEL.${kmod}_${DEVICE}${KSUSTAT}-${TIME}.zip * -x .git README.md *placeholder
+echo -e "\nCompleted in $((SECONDS / 60))m $((SECONDS % 60))s"
