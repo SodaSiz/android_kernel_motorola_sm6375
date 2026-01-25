75d2dc14d82a5972d5791d98af4f6e7f2a3e8a74
SodaSiz <despairos@proton.me>
2026-01-25 22:19:36 +0100

    Fixes for config fragments

--- build.sh
+++ build.sh
@@ -81,9 +81,9 @@ make O=out ${MAKE_ARGS} gki_defconfig
 # ==============================
 $ROOT_DIR/scripts/kconfig/merge_config.sh -m \
     $ROOT_DIR/out/.config \
-    $ROOT_DIR/vendor/holi_GKI.config \
-    $ROOT_DIR/vendor/ext_config/lineageos_moto-holi.config \
-    $ROOT_DIR/vendor/ext_config/moto-holi-bangkk.config
+    $ROOT_DIR/arch/arm64/configs/vendor/holi_GKI.config \
+    $ROOT_DIR/arch/arm64/configs/vendor/ext_config/lineageos_moto-holi.config \
+    $ROOT_DIR/arch/arm64/configs/vendor/ext_config/moto-holi-bangkk.config
 
 # ==============================
 # Finalize config
